394962e19eff2bdba2c07c789072165d1e11d13d
jiwooproity <jiwooproity@naver.com>
2021-11-14 05:44:36 +0900

    1854일의 기적 / 서브 타이틀 - 애니메이션 추가

--- Web_2/js/text_1854.js
+++ Web_2/js/text_1854.js
@@ -4,8 +4,10 @@ let plus_text = 1;
 function plus_typing() {
     if(plus_text < 1854) {
         plus_text += 1;
-        title.innerHTML = `브레이브걸스 ${plus_text}일의 기적`;
+        title.innerHTML = `+ ${plus_text}`;
     } else {
+        title.innerHTML = `브레이브걸스 ${plus_text}일의 기적`;
+        title.classList.add("fade-in-animation");
     }
 }
 
