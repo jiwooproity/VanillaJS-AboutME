e6fa6da46b2be99d11b84cb6472bd139fba6ad1a
jiwooproity <jiwooproity@naver.com>
2021-11-29 03:15:28 +0900

    메인 Title 배경 & 배경 다크 모드 구현

--- Self_Introduction/js/toggleBtn.js
+++ Self_Introduction/js/toggleBtn.js
@@ -2,9 +2,13 @@ const header__toggle = document.querySelector('.header__toggle');
 const indicator = document.querySelector('.indicator');
 const body = document.querySelector('body');
 const navbar = document.querySelector('header');
+const maskLight = document.querySelector('.image__mask-light');
+const maskDark = document.querySelector('.image__mask-dark');
 
 header__toggle.addEventListener('click', () => {
     indicator.classList.toggle('active');
+    maskLight.classList.toggle('active');
+    maskDark.classList.toggle('active');
 
     body.classList.toggle('dark');
     navbar.classList.toggle('dark');
