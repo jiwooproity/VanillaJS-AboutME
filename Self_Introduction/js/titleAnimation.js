09e219cccc087562bd5947044e04051db20a74f1
jiwooproity <jiwooproity@naver.com>
2021-11-29 21:06:35 +0900

    일부 디자인 수정 및 기능 추가

---
+++ Self_Introduction/js/titleAnimation.js
@@ -0,0 +1,10 @@
+const mainTitle = document.querySelector('.main__title');
+const mask = document.querySelector('.image__mask-light');
+
+let Y;
+
+function startTop() {
+    mainTitle.style.bottom = "50%";
+}
+
+startTop();
--- Self_Introduction/js/toggleBtn.js
+++ Self_Introduction/js/toggleBtn.js
@@ -3,7 +3,10 @@ const indicator = document.querySelector('.indicator');
 const body = document.querySelector('body');
 const navbar = document.querySelector('header');
 const maskLight = document.querySelector('.image__mask-light');
-const maskDark = document.querySelector('.image__mask-dark');
+const maskTopDark = document.querySelector('.image__mask-top-dark');
+const maskBottomDark = document.querySelector('.image__mask-bottom-dark');
+const moon = document.querySelector('.image__moon');
+const sun = document.querySelector('.image__sun');
 
 const toggleSubMenuBtn = document.querySelector('.header__toggle__menu');
 const toggleSubMenu = document.querySelector('.toggle__submenu');
@@ -11,7 +14,10 @@ const toggleSubMenu = document.querySelector('.toggle__submenu');
 header__toggle.addEventListener('click', () => {
     indicator.classList.toggle('active');
     maskLight.classList.toggle('active');
-    maskDark.classList.toggle('active');
+    maskTopDark.classList.toggle('active');
+    maskBottomDark.classList.toggle('active');
+    moon.classList.toggle('active');
+    sun.classList.toggle('active');
 
     body.classList.toggle('dark');
     navbar.classList.toggle('dark');
