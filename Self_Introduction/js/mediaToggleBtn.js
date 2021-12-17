ee75007e44330e48b7aaad5f41529ae5734d418d
jiwooproity <jiwooproity@naver.com>
2021-12-17 18:01:31 +0900

    About 세션 수정

--- Self_Introduction/js/mediaToggleBtn.js
+++ Self_Introduction/js/mediaToggleBtn.js
@@ -2,20 +2,38 @@ const subMenuList = Array.from(document.querySelectorAll('.toggle__submenu ul li
 const TitleChange2 = document.querySelector('.main__title');
 const TitleTime2 = document.querySelector('.main__time');
 
-subMenuList[2].addEventListener('click', () => {
-    let titleZindex = TitleChange2.style.zIndex;
+subMenuList.map((item, index) => {
+    item.addEventListener('click', () => {
+        if(index == 0) {
+            window.scroll({top: 0, behavior: "smooth"});
+            toggleSubMenu.classList.toggle('active');
+            headerMenuBtn.classList.toggle('active');
+        }
 
-    TitleChange2.classList.toggle('active');
-    TitleTime2.classList.toggle('active');
+        else if(index == 1) {
+            window.scroll({top: aboutSection - 78, behavior: "smooth"});
+            toggleSubMenu.classList.toggle('active');
+            headerMenuBtn.classList.toggle('active');
+        }
 
-    if(titleZindex == 2) {
-        TitleChange2.style.zIndex = "1";
-        return;
-    }
+        else if(index == 2) {
+            let titleZindex = TitleChange2.style.zIndex;
 
-    else {
-        setTimeout(() => {
-            TitleChange2.style.zIndex = "2";
-        }, 1900);
-    }
+            TitleChange2.classList.toggle('active');
+            TitleTime2.classList.toggle('active');
+            toggleSubMenu.classList.toggle('active');
+            headerMenuBtn.classList.toggle('active');
+
+            if(titleZindex == 2) {
+                TitleChange2.style.zIndex = "1";
+                return;
+            }
+
+            else {
+                setTimeout(() => {
+                    TitleChange2.style.zIndex = "2";
+                }, 1900);
+            }
+        }
+    })
 })
